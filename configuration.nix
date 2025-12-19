11ff5ade14ed6e4e4a24bb2aef18e75cb448554e
Clement <122646162+poytaytoy@users.noreply.github.com>
2025-12-19 17:39:59 -0500

    new gnome config
    Refactor NixOS configuration to enable GNOME and Wayland support, while disabling KDE Plasma. Clean up commented code and ensure unfree packages are allowed.

--- configuration.nix
+++ configuration.nix
@@ -4,8 +4,6 @@
 
 { config, pkgs, ... }:
 
-#git config --global --add safe.directory /etc/nixos (to unlock it)
-
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -45,15 +43,27 @@
   };
 
   # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  #services.displayManager.sddm.enable = true;
-  #services.desktopManager.plasma6.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+    desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverridePackages = [ pkgs.mutter ];
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer']
+      '';
+    };
+  };
 
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Enable the GNOME Desktop Environment.
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -89,13 +99,15 @@
     description = "Clement";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      kdePackages.kate
     #  thunderbird
     ];
   };
 
   # Install firefox.
   programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -110,6 +122,13 @@
     google-chrome 
     discord-ptb
     spotify
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.vitals
+    gnomeExtensions.logo-menu
+    gnomeExtensions.space-bar
+    gnomeExtensions.open-bar
+    gnome-screenshot
   ];
 
   programs.vscode = {
@@ -119,28 +138,6 @@
   };
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  #For some reason I needed to add this to get vscode working
-
-  nixpkgs.config.allowUnfree = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -212,11 +209,6 @@
     # Remove NVIDIA VGA/3D controller devices
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
   '';
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];  
 
-  stylix.base16Scheme = ”${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml”;
-
-  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  
 }
