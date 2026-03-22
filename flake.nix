3e41785eb120bd76a92a6d5f436d8131f544cccf
Clement Chung <clementcycc8@gmail.com>
2026-03-22 15:50:54 -0400

    update

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.poytaytoy = import ./home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
           }
       ];
     };
